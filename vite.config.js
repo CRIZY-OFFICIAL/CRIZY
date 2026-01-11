0ed5077b3b3418f2377e9b7a4bee2672b5c72af5
Chamath <chamabr1324@gmail.com>
2026-01-11 18:34:20 +0530

    make some changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CRIZY-Portfolio/",
+  base: "/",
   plugins: [ tailwindcss(),react()],
 })
